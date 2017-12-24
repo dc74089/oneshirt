3fdf6dfb3992be3f18553d3a95199a6aa7d39061
Dominic Canora <dominiccanora@gmail.com>
2017-12-23 23:17:11 -0500

    Add models, disable login buttons

--- trade/models.py
+++ trade/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Item(models.Model):
+    item_types = (
+    ('ts', "T-Shirt"),
+    ('hat', "Hat"),
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.IntegerField()
+    classification = models.CharField(max_length=10, choices=item_types)
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+
+class Trade(models.Model):
+    statuses = (
+    ('a', 'Accepted'),
+    ('d', 'Denied'),
+    ('p', 'Pending')
+    )
+    give = models.ManyToManyField(Item, related_name="trade_item_given")
+    take = models.ManyToManyField(Item, related_name="trade_item_gotten")
+    status = models.CharField(max_length=10, choices=statuses, default='p')

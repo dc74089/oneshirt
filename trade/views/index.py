77f1ff66f94a2a612f7de0696986816c47cd9d68
Dominic Canora <dominiccanora@gmail.com>
2018-02-06 09:41:04 -0500

    Home page display all items

--- trade/views/index.py
+++ trade/views/index.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
+from ..models import Item
 
 
 def index(request):
-    return render(request, 'trade/index_home.html')
+    ctx = {}
+    cards = []
+
+    for item in Item.objects.all():
+        cards.append(render_to_string("trade/partial/item_card.html", {"item": item}))
+
+    ctx['cards'] = cards
+
+    return render(request, 'trade/index_home.html', ctx)

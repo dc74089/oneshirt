7684ba43a868229c1c673b1bf491bd4721713082
Dominic Canora <dominiccanora@gmail.com>
2018-02-10 11:48:23 -0500

    Non-functional search page

--- trade/views/search.py
+++ trade/views/search.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from ..models import Item
+
 
 def search(request):
-    return render(request, 'trade/search.html')
+    ctx = {}
+    ctx['item_types'] = Item.item_types
+
+    return render(request, 'trade/search.html', ctx)

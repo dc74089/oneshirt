7934e581eb9bc4d3dc47e151111f2c0406181925
Dominic Canora <dominiccanora@gmail.com>
2018-02-28 09:37:33 -0500

    Remove HTML email messages

--- trade/email.py
+++ trade/email.py
@@ -2,6 +2,7 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
 
+# TODO: Fix html message not displaying in gmail
 def trade_mail(trade):
     ctx = {'trade': trade}
     html = render_to_string('trade/email/trade_notification.html', ctx)
@@ -10,7 +11,7 @@ def trade_mail(trade):
     )
     subject = "Trade offer for your %s" % trade.take
 
-    send_mail(subject, plain, "webmaster@mail.frcshirt.trade", [trade.take.owner.email], html_message=html)
+    send_mail(subject, plain, "webmaster@mail.frcshirt.trade", [trade.take.owner.email])
 
 
 def trade_accepted_mail(trade):
@@ -25,4 +26,4 @@ def trade_accepted_mail(trade):
             )
     subject = "Trade offer accepted!"
 
-    send_mail(subject, plain, "webmaster@mail.frcshirt.trade", [trade.give.owner.email], html_message=html)
+    send_mail(subject, plain, "webmaster@mail.frcshirt.trade", [trade.give.owner.email])

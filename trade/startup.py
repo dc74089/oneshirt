fa7ee89287217693a1306c4c7f5cce416f1ca0ca
Dominic Canora <dominiccanora@gmail.com>
2018-03-16 15:24:24 -0400

    Fix image path error??? Hopefully.

--- trade/startup.py
+++ trade/startup.py
@@ -10,6 +10,6 @@ from .models import Item
 @receiver(post_save, sender=Item, dispatch_uid="rotate_item_image")
 def update_image(sender, instance, **kwargs):
     if instance.photo:
-        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
         fullpath = BASE_DIR + instance.photo.url
         utils.rotate_image(fullpath)
